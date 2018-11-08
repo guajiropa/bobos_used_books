42478c0e6eec04e014e814cf079329bf94a0e71c
Robert James Patterson <guajiropa@hotmail.com>
2018-11-08 07:28:25 -0500

    Added bookstore objects to the admin site

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,11 @@
+"""
+AUTHOR      :   Robert James Patterson
+DATE        :   11/08/2018
+SYNOPSIS    :   Work thru file for 'Mastering Django: Core'
+"""
 from django.contrib import admin
+from .models import Publisher, Author, Book
 
-# Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
