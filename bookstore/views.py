30181ceee46e3416bae72a184aa8ea8192da8d52
guajiropa <guajiropa@hotmail.com>
2018-11-17 08:17:27 -0500

    catch-up commit

--- bookstore/views.py
+++ bookstore/views.py
@@ -6,6 +6,7 @@ SYNOPSIS    :   Work-thru file for 'Mastering Django: Core'.
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from bookstore.models import Book
 
 
 def search_form(request):
@@ -13,10 +14,16 @@ def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
+    
+    errors = []
 
     if 'q' in request.GET:
-        message = "You searched for:  %r ." % request.GET['q']
-    else:
-        message = "You submitted an empty form."
-
-    return HttpResponse(message)
+        q = request.GET['q']
+        if not q: 
+            errors.append('Please enter a valid search term.')
+        elif len(q) > 20:
+            errors.append('Please enter a search term of 20 characters or less.')
+        else: 
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'search_results.html', {'books': books, 'query': q})
+        return render (request, 'search_form.html', {'errors': errors})
